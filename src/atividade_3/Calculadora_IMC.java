397a0419dd245418dd59490f5699a65db490ec48
Cleverson Mendes Faria <clemendes1@hotmail.com>
2020-05-23 12:28:05 -0300

    Update Calculadora_IMC.java

--- src/atividade_3/Calculadora_IMC.java
+++ src/atividade_3/Calculadora_IMC.java
@@ -10,15 +10,15 @@ import javax.swing.JOptionPane;
 
 /*  QUADRO DE REFERENCIA - RESULTADOS IMC
 -----------------------------------------------
-[   Resultado  ] 	    [    Situação   ]	
+[   Resultado  ] 	    [    SituaÃ§Ã£o   ]	
 -----------------------------------------------			
  Abaixo de 17			Muito abaixo do peso
  Entre 17 e 18,49		Abaixo do peso
- Entre 18,50 e 24,99	Peso normal
+ Entre 18,50 e 24,99		Peso normal
  Entre 25 e 29,99		Acima do peso
  Entre 30 e 34,99		Obesidade I
  Entre 35 e 39,99		Obesidade II (severa)
- Acima de 40			Obesidade III (mórbida) 
+ Acima de 40			Obesidade III (mÃ³rbida) 
  ---------------------------------------------- */
 
 public class Calculadora_IMC {
@@ -37,7 +37,7 @@ public class Calculadora_IMC {
 				String entrada2 = JOptionPane.showInputDialog("Informe a sua altura em (cm): \nExemplo: 175");
 				if (entrada2 != null)
 					altura = Double.parseDouble(entrada2);
-					// CHAMA O MÉTODO P/ CALCULAR O IMC
+					// CHAMA O MÃ‰TODO P/ CALCULAR O IMC
 					calcula_imc(peso, altura/100);
 		// Erro
 		} catch (Exception e) {
@@ -59,27 +59,27 @@ public class Calculadora_IMC {
 		
 		if (imc < 17.00)
 			JOptionPane.showMessageDialog(null, "IMC: [" + String.format("%.2f", imc) + "]"
-					+ "\nSituação: Muito abaixo do peso!", "Resultado", 1);
+					+ "\nSituaÃ§Ã£o: Muito abaixo do peso!", "Resultado", 1);
 		else if (imc >= 17.00 && imc <= 18.49)
 			JOptionPane.showMessageDialog(null, "IMC: [" + String.format("%.2f", imc) + "]"
-					+ "\nSituação: Abaixo do peso!", "Resultado", 1);
+					+ "\nSituaÃ§Ã£o: Abaixo do peso!", "Resultado", 1);
 		else if (imc >= 18.50 && imc <= 24.99)
 			JOptionPane.showMessageDialog(null, "IMC: [" + String.format("%.2f", imc) + "]"
-					+ "\nSituação: Peso normal!", "Resultado", 1);
+					+ "\nSituaÃ§Ã£o: Peso normal!", "Resultado", 1);
 		else if (imc >= 25.00 && imc <= 29.99)
 			JOptionPane.showMessageDialog(null, "IMC: [" + String.format("%.2f", imc) + "]"
-					+ "\nSituação: Acima do peso!", "Resultado", 1);
+					+ "\nSituaÃ§Ã£o: Acima do peso!", "Resultado", 1);
 		else if (imc >= 30.00 && imc <= 34.99)
 			JOptionPane.showMessageDialog(null, "IMC: [" + String.format("%.2f", imc) + "]"
-					+ "\nSituação: Obesidade I!", "Resultado", 1);
+					+ "\nSituaÃ§Ã£o: Obesidade I!", "Resultado", 1);
 		else if (imc >= 35.00 && imc <= 39.99)
 			JOptionPane.showMessageDialog(null, "IMC: [" + String.format("%.2f", imc) + "]"
-					+ "\nSituação: Obesidade II (severa)!", "Resultado", 1);
+					+ "\nSituaÃ§Ã£o: Obesidade II (severa)!", "Resultado", 1);
 		else if (imc >= 40.00)
 			JOptionPane.showMessageDialog(null, "IMC: [" + String.format("%.2f", imc) + "]"
-					+ "\nSituação: Obesidade III (mórbida)!", "Resultado", 1);
+					+ "\nSituaÃ§Ã£o: Obesidade III (mÃ³rbida)!", "Resultado", 1);
 		else
-			JOptionPane.showMessageDialog(null, "Valores informados inválidos!", "Erro", 0);
+			JOptionPane.showMessageDialog(null, "Valores informados invÃ¡lidos!", "Erro", 0);
 	}
 
 }
